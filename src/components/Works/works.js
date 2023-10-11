fd06d48ab2ef63f0d03e1708a8fc531ac3df5699
Jagjot Saini <jsaini24@my.whitworth.edu>
2023-10-10 23:34:39 -0700

    test1

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './works.css';
+import Linkdin from '../../assets/LinkedinLogo.png';
+import Github from '../../assets/GithubLogo.png';
+import Overleaf from '../../assets/Overleaf_Logo.jpg'
 
 const works = () => {
   return (
    <section id="works">
     <h2 className='workstitle'>My Portfolio</h2>
     <span className='WorkDesc'>I take prife in making this website</span>
-    
+    <div className="worksImdgs">
+        <img src={Overleaf} alt="Overleaf" className='OverleafLogo'/>
+        <img src={Github} alt="Github" className='GithubLogo'/>
+        <img src="" alt="" className=''/>
+        <img src="" alt="" className=''/>
+        <img src="" alt="" className=''/>
+        <img src="" alt="" className=''/>    
+    </div>
    </section>
   )
 }
